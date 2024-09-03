924a916f5a4f2d03673183e5069a8518dbaa8260
zahrapirjani <zahrapirjani1375@gmail.com>
2024-09-04 01:14:36 +0330

    rx and obs (13 shahrivar)

---
+++ getx13_shahrivar/lib/model/product.dart
@@ -0,0 +1,6 @@
+class Product{
+  String name;
+  String price;
+  String off;
+  Product({required this.name,required this.price,required this.off});
+}
---
+++ getx13_shahrivar/lib/view/main_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx13_shahrivar/model/product.dart';
+class MainScreen extends StatelessWidget{
+Rx<Product> product = Product(name: "benz", price: "100T", off: "2%").obs;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment:MainAxisAlignment.center,
+          children: [
+            Obx(((){
+              return Column(
+                children: [
+                  Text("Counter: ${product.value.name}"),
+                  Text("Counter: ${product.value.price}"),
+                  Text("Counter: ${product.value.off}"),
+                ],
+              );
+            })),
+            ElevatedButton(style:ButtonStyle(backgroundColor:WidgetStatePropertyAll<Color>(Colors.green) ),onPressed: (){
+              product.update((value){
+                value!.name="pride";
+                value!.price="2T";
+                value!.off="3%";
+              });
+            }, child: Icon(Icons.update,color: Colors.white,),)
+          ],
+        ),
+      ),
+   );
+  }
+  
+}
